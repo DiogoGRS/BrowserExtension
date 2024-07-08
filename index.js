f28d908a1073e25cfdb6428211e05a8658c38aee
Diogo <diogogrsantos1@gmail.com>
2024-07-08 20:33:07 -0300

    new changes in lists and links

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 
-let myLeads = ["claudio", "saulio", "bergstein"]
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEL = document.getElementById("ul-el") 
 
 inputBtn.addEventListener( "click", function() {
 myLeads.push(inputEl.value)
-console.log(myLeads)
+inputEl.value = ""
+renderLeads()
 })
 
+function renderLeads(){
+let listItens = ""
 for (let i = 0; i < myLeads.length; i++) {
-    ulEL.textContent += " " + (myLeads[i]) 
+    listItens += `
+        <li>
+           <a target='_blank' href = '${(myLeads[i])}'> 
+             ${myLeads[i]} 
+           </a>
+        </li>
+        
+        `
+    }
+  ulEL.innerHTML = listItens
 }
+
