2762511bdc4ce673932f48fbc3c201bb8d971590
Diogo <diogogrsantos1@gmail.com>
2024-07-07 22:33:51 -0300

    creating a ul of leads

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 
-let myLeads = []
+let myLeads = ["claudio", "saulio", "bergstein"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const ulEL = document.getElementById("ul-el") 
 
 inputBtn.addEventListener( "click", function() {
-myLeads.push("www.myleads.com")
+myLeads.push(inputEl.value)
 console.log(myLeads)
 })
+
+for (let i = 0; i < myLeads.length; i++) {
+    ulEL.textContent += " " + (myLeads[i]) 
+}
