bc60ed68bfde99ec1c2be91017ab641b7a98b1f3
Diogo Gonçalves Rosa <45168156+DiogoGRS@users.noreply.github.com>
2024-07-10 16:20:55 -0300

    final commit

--- index.js
+++ index.js
@@ -3,25 +3,55 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEL = document.getElementById("ul-el") 
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const tabBtn = document.getElementById("tab-btn")
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage 
+    render(myLeads)
+}
+
+tabBtn.addEventListener("click", function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+})
+
+function render(leads){
+    let listItens = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItens += `
+            <li>
+               <a target='_blank' href = '${leads[i]}'> 
+                 ${leads[i]} 
+               </a>
+            </li>
+            
+            `
+        }
+      ulEL.innerHTML = listItens
+    } 
+
+deleteBtn.addEventListener("dblclick", function(){
+    myLeads = []
+    localStorage.clear()
+    render(myLeads)
+})
 
 inputBtn.addEventListener( "click", function() {
 myLeads.push(inputEl.value)
 inputEl.value = ""
-renderLeads()
+  
+localStorage.setItem("myLeads", JSON.stringify(myLeads))
+
+render(myLeads)
+
+console.log(localStorage.getItem("myLeads"))
+
 })
 
-function renderLeads(){
-let listItens = ""
-for (let i = 0; i < myLeads.length; i++) {
-    listItens += `
-        <li>
-           <a target='_blank' href = '${(myLeads[i])}'> 
-             ${myLeads[i]} 
-           </a>
-        </li>
-        
-        `
-    }
-  ulEL.innerHTML = listItens
-}
+
 
